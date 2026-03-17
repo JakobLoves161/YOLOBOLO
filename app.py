3fcffeeb0092c1d05a8f7c171ca8db075c0f9436
JakobLoves161 <friedrbe@katharineum.de>
2026-03-17 14:33:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -27,4 +27,3 @@ if uploaded_file is not None:
     st.success("Top-Ergebnisse:")
     for i, res in enumerate(results[:5], 1):
         st.write(f"{i}. **{res['label']}** – {res['score']:.1%} sicher")
-print "hello world"
